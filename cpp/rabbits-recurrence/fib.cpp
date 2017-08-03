bf8355571f95870a1ec8543f417bdc95ba906660
akasi311 <aaronabraham311@gmail.com>
2017-08-03 10:20:48 -0600

    Implementing recursive solution

--- cpp/rabbits-recurrence/fib.cpp
+++ cpp/rabbits-recurrence/fib.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+int recurrence (int n, int k);
+
 int main()
 {
     int months, litter;
@@ -11,21 +13,25 @@ int main()
     cin >> months;
     cin >> litter;
     
-    // Initializing more variables
-    int tooYoung = 1; // Rabbits that cannot reproduce
-    int ready = 0; // Rabbits that can reproduce
-    int newChildren; // Rabbits born every month
-    
-    // Looping through months and updating rabbit population
-    for (int i = 1; i < months; i ++)
-    {
-        newChildren = ready * litter;
-        
-        ready += tooYoung;
-        tooYoung = newChildren;
-    }
-    
-    cout << tooYoung + ready << endl; // Outputting number of rabbits
+    cout << recurrence(months, litter) << endl; // Calling function to output total number of rabbits
     
     return 0;
+}
+
+// Logic: 1st and 2nd month will always have 1 rabbit pair. Afterwards, total number of rabbits will be equal to rabbits born
+// one month ago plus the rabbits born on that month (rabbits born 2 months ago * litter size)
+int recurrence (int n, int k)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else if (n == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return recurrence(n-1, k) + (recurrence(n - 2, k) * k)
+    }
 }
