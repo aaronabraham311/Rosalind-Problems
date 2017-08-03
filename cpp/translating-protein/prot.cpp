467e453ce0a2ee7d07b65b0bbc208ee5a452ab86
akasi311 <aaronabraham311@gmail.com>
2017-08-03 14:39:05 -0600

    Create prot.cpp
    Takes codons of RNA and turns it into string of amino acids, commonly known as proteins

---
+++ cpp/translating-protein/prot.cpp
@@ -0,0 +1,24 @@
+// INCOMPLETE
+
+#include <iostream> 
+#include <string>
+using namespace std;
+
+// Logic: Substrings of 3 --> compare to 2D array that takes codon and gives protein letter --> puts in new string using .append()
+
+int main()
+{
+    string rnaSequence, aaSequence, codon;
+    string codonArray[20][2]; // Put in list
+    int answer[100]; // Stores answer
+    
+    cin >> rnaSequence;
+    
+    for (int i = 0; i < rnaSequence.length(); i += 3)
+    {
+        codon = rnaSequence[i] + rnaSequence[i + 1] + rnaSequence[i + 2]
+        
+    }
+    
+    return 0;
+}
