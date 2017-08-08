677575d4e5a08272fcac42574e95c39bc4f2e5ad
akasi311 <aaronabraham311@gmail.com>
2017-08-08 08:27:36 -0600

    completed if-elseif statements

--- cpp/translating-protein/prot.cpp
+++ cpp/translating-protein/prot.cpp
@@ -1,4 +1,3 @@
-// INCOMPLETE
 #include <iostream>
 #include <string>
 
@@ -13,8 +12,6 @@ int main()
     string temp;
     
     cin >> rnaSequence;
-    
-    cout << rnaSequence.substr(0, 3) << endl;
     
     for (int i = 0; i < rnaSequence.length(); i += 3)
     {
@@ -32,7 +29,7 @@ int main()
         {
             protein += "S";
         }
-        else if (temp == "UAU" || temp == "UAC") // Skipped stop codons
+        else if (temp == "UAU" || temp == "UAC")
         {
             protein += "Y";
         }
@@ -43,6 +40,79 @@ int main()
         else if (temp == "UGG")
         {
             protein += "W";
+        }
+        else if (temp == "CUU" || temp == "CUC" || temp == "CUA" || temp == "CUG")
+        {
+            protein += "L";
+        }
+        else if (temp == "CCU" || temp == "CCC" || temp == "CCA" || temp == "CCG")
+        {
+            protein += "P";
+        }
+        else if (temp == "CAU" || temp == "CAC")
+        {
+            protein += "H";
+        }
+        else if (temp == "CAA" || temp == "CAG")
+        {
+            protein += "Q";
+        }
+        else if (temp == "CGU" || temp == "CGC" || temp == "CGA" || temp == "CGG")
+        {
+            protein +=  "R";
+        }
+        else if (temp == "AUU" || temp == "AUC" || temp == "AUA")
+        {
+            protein += "I";
+        }
+        else if (temp == "AUG")
+        {
+            protein += "M";
+        }
+        else if (temp == "ACU" || temp == "ACC" || temp == "ACA" || temp == "ACG")
+        {
+            protein += "T";
+        }
+        else if (temp == "AAU" || temp == "AAC")
+        {
+            protein += "N";
+        }
+        else if (temp == "AAA" || temp == "AAG")
+        {
+            protein += "K";
+        }
+        else if (temp == "AGU" || temp == "AGC")
+        {
+            protein += "S";
+        }
+        else if (temp == "AGA" || temp == "AGG")
+        {
+            protein += "R";
+        }
+        else if (temp == "GUA" || temp == "GUU" || temp == "GUG" || temp == "GUC")
+        {
+            protein += "V";
+        }
+        else if (temp == "GCU" || temp == "GCG" || temp == "GCA" || temp == "GCC")
+        {
+            protein += "A";
+        }
+        else if (temp == "GAU" || temp == "GAC")
+        {
+            protein += "D";
+        }
+        else if (temp == "GAA" || temp == "GAG")
+        {
+            protein += "E";
+        }
+        else if (temp == "GGU" || temp == "GGA" || temp == "GGC" || temp == "GGG")
+        {
+            protein += "G";
+        }
+        else if (temp == "UAA" || temp == "UAG" || temp == "UGA")
+        {
+            cout << protein << endl;
+            protein.clear();
         }
     }
     
