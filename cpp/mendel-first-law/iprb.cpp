cbce9b964eac4e7c5ba8dea38913811fed641be7
akasi311 <aaronabraham311@gmail.com>
2017-08-17 08:24:06 -0600

    solution code

--- cpp/mendel-first-law/iprb.cpp
+++ cpp/mendel-first-law/iprb.cpp
@@ -1,5 +1,3 @@
-// INCOMPLETE
-
 #include <iostream>
 #include <cmath>
 
@@ -16,20 +14,11 @@ int main()
     cin >> k >> m >> n;
     sum = k + m + n;
     
-    cout << 1 - (0.25 * probs(m, m, sum)) - (0.5 * probs(m, n, sum)) - (probs(n, n, sum)) << endl; // Outputs chance
+    // Calculating probabilities
+    float twoRecessive = (n/sum) * ((n - 1)/(sum - 1));
+    float twoHeteros = (m/sum) * ((m - 1)/(sum - 1));
+    float heteroRecessive = (m/sum) * (n/(sum - 1)) + (n/sum) * (m / (sum - 1));
     
+    cout << 1 - (twoRecessive + twoHeteros * 0.25 + heteroRecessive * 0.5) << endl; // Outputting total probability
     
     return 0;
-}
-
-float probs (float firstIndividual, float secondIndividual, float sum)
-{
-    if (firstIndividual == secondIndividual)
-    {
-        return (firstIndividual/sum) * ((secondIndividual - 1)/(sum - 1));
-    }
-    else
-    {
-        return (firstIndividual/sum) * (secondIndividual/(sum - 1));
-    }
-}
