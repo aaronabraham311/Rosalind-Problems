a52cccdd7ba928d67f243fb068b283e837632c62
Aaron Abraham <aaronabraham311@gmail.com>
2017-08-18 18:02:27 -0600

    winning solution
    Used next_permutation function in algorithm library

--- cpp/enumerating-gene-order/perm.cpp
+++ cpp/enumerating-gene-order/perm.cpp
@@ -1,18 +1,35 @@
-// INCOMPLETE
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int factorial(int number);
+void permute (vector <int> a, int l, int r);
 
 int main()
 {
     int n;
+    vector <int> allFactors;
     
     cin >> n;
 
     cout << factorial(n) << endl;
     
+    for (int i = 0; i < n; i ++) // Populating vector array
+    {
+        allFactors.push_back(i + 1);
+    }
+    
+    do
+    {
+        for (int i = 0; i < allFactors.size(); i ++)
+        {
+            cout << allFactors[i] << " ";
+        }
+        
+        cout << endl;
+    } while (next_permutation(allFactors.begin(), allFactors.end()));
     
     
     return 0;
@@ -28,4 +45,6 @@ int factorial(int number) // Recursive factorial function
     {
         return number * factorial(number - 1);
     }
+    
 }
+
