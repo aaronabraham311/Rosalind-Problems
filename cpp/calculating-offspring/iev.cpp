b94d4c0232fc5d6da82b3aa397923dadf16be89b
Aaron Abraham <aaronabraham311@gmail.com>
2017-08-21 19:15:04 -0600

    winning solution

--- cpp/calculating-offspring/iev.cpp
+++ cpp/calculating-offspring/iev.cpp
@@ -1,31 +1,26 @@
-// INCOMPLETE
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    float couples[6];
-    float total;
+    float couples[6]; // Array of population numbers
     
+    // Inputting population
     for (int i = 0; i < 6; i ++)
     {
         cin >> couples[i];
     }
     
-    float firstPheno = couples[0] * 2;
-    float secondPheno = couples[1] * 2;
-    float thirdPheno = couples[2] * 2;
-    float fourthPheno = couples[3] * 2 * 0.75;
-    float fifthPheno = couples[4] * 2 * 0.5;
+    // Calculating probabilities
+    float firstPheno = couples[0] * 1;
+    float secondPheno = couples[1] * 1;
+    float thirdPheno = couples[2] * 1;
+    float fourthPheno = couples[3] * 0.75;
+    float fifthPheno = couples[4] * 0.5;
     
-    for (int i = 0; i < 6; i ++)
-    {
-        total += couples[i] * 2;
-    }
+    float finalProb = firstPheno + secondPheno + thirdPheno + fourthPheno + fifthPheno; // Calculating total probability
     
-    float output = total * ((firstPheno + secondPheno + thirdPheno + fourthPheno + fifthPheno) / total);
-    
-    cout << output  << endl;
+    cout << finalProb * 2 << endl; // Outputting final result. Multiply by 2 as each couple has two kids
     
 }
