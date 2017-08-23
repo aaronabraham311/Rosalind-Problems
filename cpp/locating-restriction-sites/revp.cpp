53802827675f4518e4e9e4cb9a654073af4f8e79
Aaron Abraham <aaronabraham311@gmail.com>
2017-08-23 17:42:50 -0600

    Winning solution
    Corrected substring and restriction bugs

--- cpp/locating-restriction-sites/revp.cpp
+++ cpp/locating-restriction-sites/revp.cpp
@@ -1,36 +1,41 @@
-// INCOMPLETE
+// Including libraries
 #include <iostream>
 #include <string>
 #include <algorithm>
 
 using namespace std;
 
+// Protyping functions
 bool complementCheck(string check);
 char complementCharacter(char input);
 
+// Logic: Go on each character --> find complement character further down string  --> make substring and check reverse complement --> palindrome program
 int main()
 {
+    // Declaring all variables
     string dnaString;
     string complementString;
-    int index1, index2, length;
+    int length;
     
-    getline(cin, dnaString);
+    // Getting all input. NOTE: Use FASTA format
+    cin >> dnaString;
     
-    length = dnaString.length();
+    length = dnaString.length(); // Getting length of string
     
+    // Puts first character and finds next character that is a complement
     for (int i = 0; i < length; i ++)
     {
-        for (int j = i + 1; j < length; j ++)
+        for (int j = i + 1; j < length; j ++) // Using nested loop to find next complement
         {
-            if (dnaString[i] == complementCharacter(dnaString[j]))
+            if (dnaString[i] == complementCharacter(dnaString[j])) // Checking if characters are equal complements
             {
-                if (j - i >= 4 || j - i <= 12)
+                if (j - i + 1 >= 4 && j - i + 1 <= 12) // Putting restriction
                 {
-                    complementString = dnaString.substr(i, j - i);
+                    complementString = dnaString.substr(i, j - i + 1); // Making a substring
                     
-                    if (complementCheck(complementString) == true)
+                    if (complementCheck(complementString) == true) // Checks if substring is a reverse palindrome
                     {
-                        cout << i << " " << j - i << endl;
+                        cout << i + 1 << " " << j - i + 1 << endl; // Output
                     }
                 }
             }
@@ -80,6 +85,7 @@ bool complementCheck(string check)
     return test;
 }
 
+// Returns complement character
 char complementCharacter(char input)
 {
     if (input == 'A')
